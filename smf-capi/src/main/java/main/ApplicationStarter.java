d55a41cff358a0debd932e96db566ee702b8d655
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-12-07 16:31:27 +0100

    1. Added .gitignore file 2. Added test controller and main class

---
+++ smf-capi/src/main/java/main/ApplicationStarter.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by cipriach on 07.12.2015.
+ */
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+
+}
---
+++ smf-capi/src/main/java/main/model/UserResponse.java
@@ -0,0 +1,17 @@
+package main.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * Created by cipriach on 07.12.2015.
+ */
+@Builder
+@Getter
+@Setter
+public class UserResponse implements Serializable{
+    private boolean isOk;
+}
