58858784e2d2d95c2094ebb48123bd426319f34e
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-12-11 15:20:06 +0100

    Added deployable war on local tomcat

--- smf-capi/src/main/java/com/smf/main/ApplicationStarter.java
+++ smf-capi/src/main/java/com/smf/main/ApplicationStarter.java
@@ -2,12 +2,19 @@ package com.smf.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by cipriach on 07.12.2015.
  */
 @SpringBootApplication
-public class ApplicationStarter {
+public class ApplicationStarter extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationStarter.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
