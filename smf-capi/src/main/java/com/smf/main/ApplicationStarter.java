2ae4ce656bcf61a49aa8c533229eb40ed859aeb2
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-12-24 12:13:38 +0100

    Added possible ui improvement. To test it at home

--- smf-capi/src/main/java/com/smf/main/ApplicationStarter.java
+++ smf-capi/src/main/java/com/smf/main/ApplicationStarter.java
@@ -2,13 +2,14 @@ package com.smf.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by cipriach on 07.12.2015.
  */
 @SpringBootApplication
-public class ApplicationStarter extends SpringBootServletInitializer{
+public class ApplicationStarter extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
